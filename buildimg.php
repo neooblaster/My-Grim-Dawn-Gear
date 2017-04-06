a0aed3aee2c5170d49afb9f505606b47dae88b6c
Neoblaster <neo-blaster@hotmail.fr>
2017-04-06 17:33:25 +0000

    Finishing of image builder which intehrate component & enchant

--- buildimg.php
+++ buildimg.php
@@ -8,7 +8,7 @@
 /** ---																																					--- **
 /** ---		AUTEUR 	: Nicolas DUPRE																											--- **
 /** ---																																					--- **
-/** ---		RELEASE	: xx.xx.2016																												--- **
+/** ---		RELEASE	: 06.04.2016																												--- **
 /** ---																																					--- **
 /** ---		VERSION	: 1.0																															--- **
 /** ---																																					--- **
@@ -18,7 +18,7 @@
 /** --- 														-----------------------------														--- **	
 /** ---																																					--- **
 /** ---																																					--- **
-/** ---		VERSION 1.0 : xx.xx.2016																											--- **
+/** ---		VERSION 1.0 : 06.04.2016																											--- **
 /** ---		------------------------																											--- **
 /** ---			- Première release																												--- **
 /** ---																																					--- **
@@ -69,6 +69,7 @@
 	$qualities;	// ARRAY		:: Liste des différente qualitée
 	$slots;		// ARRAY		:: Donnée stockée concernant les slots
 	$items;		// ARRAY		:: Liste des objets à charger/chargé
+	$empties;	// ARRAY		:: Liste des slots avec l'état vide ou non
 
 	$gear_size;	// ARRAY		:: Dimmension du panneau "GEAR"
 
@@ -82,6 +83,7 @@
 /** > Initialisation des variables **/
 	$qualities = Array("", "COMMON", "MAGIC", "RARE", "ELITE", "LEGENDARY", "RELIC", "COMP", "ENCHANT");
 	$slots = Array();
+	$empties = Array();
 	$items = Array(
 		"LIST" => Array(),
 		"DATA" => Array()
@@ -111,18 +113,26 @@
 		foreach($faBuild as $col => $value){
 			// S'il s'agit d'un slot SLOT_$BASE_$SPEC
 			if(preg_match("#^SLOT_#", $col)){
+				// Récupérer la base du slot
+				$slot_base_name = substr($col, 0, strrpos($col, "_"));
+				$slot_spec = substr($col, strrpos($col, "_") + 1);
+				
+				// Engistré le slots dans les empties
+				if(!array_key_exists($slot_base_name, $empties)) $empties[$slot_base_name] = true;
+				
 				// Si la valeur est différente de 0
 				if(intval($value)){
-					// Récupérer la base du slot
-					$slot_base_name = substr($col, 0, strrpos($col, "_"));
-					$slot_spec = substr($col, strrpos($col, "_") + 1);
-					
 					if(!isset($slots[$slot_base_name])) $slots[$slot_base_name] = Array();
 					
 					$slots[$slot_base_name][$slot_spec] = $value;
 					
 					$items["LIST"][] = $value;
 					$items["DATA"][$value] = Array();
+					
+					// Si c'est la spec ITEM alors le slot n'est pas vide
+					if($slot_spec === "ITEM"){
+						$empties[$slot_base_name] = false;
+					}
 				}
 			}
 		}
@@ -176,8 +186,38 @@ $gear_img = imagecreatefrompng(BACKGROUND_GEAR_PATH);
 imagecopy($img, $gear_img, BACKGROUND_GEAR_X, BACKGROUND_GEAR_X, 0, 0, imagesx($gear_img), imagesy($gear_img));
 
 
+
+/** > Remplissage des slots VIDE **/
+foreach($empties as $slot => $empty){
+	if(!$empty) continue;
+	
+	$slot_x = constant($slot."_X");
+	$slot_y = constant($slot."_Y");
+	
+	$image_file = "Images/Backgrounds/$slot.png";
+	
+	// TEMPORAIRE : ignoré les _2
+	if(preg_match("#_2$#i", $slot)) continue;
+	
+	//--- Si l'image n'est pas trouvé, alors on passe
+	if(!file_exists($image_file)) continue;
+	
+	//--- Créer une copie du slot pour intégration
+	$slot_image = imagecreatefrompng($image_file);
+	
+	//--- Intégrationde l'image
+	imagecopy($img, $slot_image, $slot_x, $slot_y, 0, 0, imagesx($slot_image), imagesy($slot_image));
+	
+	//--- Destruction de la ressource
+	imagedestroy($slot_image);
+}
+
+
+
 /** > Intégration des objets **/
-foreach($slots as $slot => $spec){
+foreach($slots as $slot => $specs){
+	//pprints("ITEMS:", $items, "SLOTS:", $slots, "SLOT:", $slot, "SPEC:", $spec);
+	
 	// #1. Récupérer les paramètres du slots
 	$slot_x = constant($slot."_X");
 	$slot_y = constant($slot."_Y");
@@ -185,9 +225,10 @@ foreach($slots as $slot => $spec){
 	$slot_h = constant($slot."_H");
 	
 	
+	
 	// #2. Commencer par le dégradé correspondant à la qualité de l'objet
 	//--- Récupération de l'ID quality
-	$quality = $items["DATA"][$spec["ITEM"]]["QUALITY"];
+	$quality = $items["DATA"][$specs["ITEM"]]["QUALITY"];
 	//--- Récupération de la couleur
 	$quality_color = constant("COLOR_{$qualities[$quality]}")."B2"; // B2 = alpha value
 	//--- Application du Background
@@ -196,9 +237,11 @@ foreach($slots as $slot => $spec){
 	
 	// #3. Commencer par inséré l'objet
 	//--- Récupération du tag (nom de fichier)
-	$tag = $items["DATA"][$spec["ITE%"]]["TAG"];
+	$tag = $items["DATA"][$specs["ITEM"]]["TAG"];
+	
 	//--- Sécurisation si le fichier n'existe pas
 	$item_file = (file_exists("Images/Items/$tag.png")) ? "Images/Items/$tag.png" : "Images/Items/NotFound.png";
+	
 	//--- "Imagisation"
 	$item_img = imagecreatefrompng($item_file);
 	
@@ -236,8 +279,75 @@ foreach($slots as $slot => $spec){
 	imagedestroy($item_img);
 	
 	
-	// #4. Insérer les attachements (comp & enchant)
+	// #4. Insérer le composant (si présent)
+	if($specs["COMP"]){
+		//--- Récupération du tag (nom de fichier)
+		$comp = $items["DATA"][$specs["COMP"]]["TAG"];
+		
+		//--- Sécurisation si le fichier n'existe pas
+		$comp_file = (file_exists("Images/Items/$comp.png")) ? "Images/Items/$comp.png" : "Images/Items/NotFound.png";
+		
+		//--- "Imagisation"
+		$comp_img = imagecreatefrompng($comp_file);
+		$comp_img = imagescale($comp_img, (imagesx($comp_img) * 0.75));
+		
+		//--- Déterminer les coordonnée d'emplacement de l'image (en bas à droit)
+		$comp_x = 0;
+		$comp_y = 0;
+		//───┐ Départ via l'emplacement du slot
+		$comp_x += $slot_x;
+		$comp_y += $slot_y;
+		//───┐ Aller en bas à droite
+		$comp_x += $slot_w;
+		$comp_y += $slot_h;
+		//───┐ Décaller vers la gauche de la largeur de l'image du composant et autant en hauteur
+		$comp_x -= imagesx($comp_img);
+		$comp_y -= imagesy($comp_img);
+		
+		
+		//--- Inséré l'image
+		imagecopy($img, $comp_img, $comp_x, $comp_y, 0, 0, imagesx($comp_img), imagesy($comp_img));
+		
+		//--- Détruire l'objet
+		imagedestroy($comp_img);
+	}
+	
+	
+	// #5. Insérer l'enchant (si présent)
+	if($specs["ENCHANT"]){
+		//--- Récupération du tag (nom de fichier)
+		$enchant = $items["DATA"][$specs["ENCHANT"]]["TAG"];
+		
+		//--- Sécurisation si le fichier n'existe pas
+		$enchant_file = (file_exists("Images/Items/$enchant.png")) ? "Images/Items/$enchant.png" : "Images/Items/NotFound.png";
+		
+		//--- "Imagisation"
+		$enchant_img = imagecreatefrompng($enchant_file);
+		$enchant_img = imagescale($enchant_img, (imagesx($enchant_img) * 0.75));
+		
+		//--- Déterminer les coordonnée d'emplacement de l'image (en bas à droit)
+		$enchant_x = 0;
+		$enchant_y = 0;
+		//───┐ Départ via l'emplacement du slot
+		$enchant_x += $slot_x;
+		$enchant_y += $slot_y;
+		//───┐ Aller en bas à gauche
+		$enchant_x += 0;
+		$enchant_y += $slot_h;
+		//───┐ Décaller vers le haut de la hauteur de l'image de l'enchant
+		$enchant_x -= 0;
+		$enchant_y -= imagesy($enchant_img);
+		
+		
+		//--- Inséré l'image
+		imagecopy($img, $enchant_img, $enchant_x, $enchant_y, 0, 0, imagesx($enchant_img), imagesy($enchant_img));
+		
+		//--- Détruire l'objet
+		imagedestroy($enchant_img);
+	}
 }
+
+
 
 
 /** > Finalisation de l'image **/
