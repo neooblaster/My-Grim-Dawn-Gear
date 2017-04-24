30f633f0cc62f1e28cca8d6301fcfff578803a97
Neoblaster <neo-blaster@hotmail.fr>
2017-04-24 21:25:49 +0000

    Add loading_attributes_names function

---
+++ Processors/Functions/Index/load_attributes_names.php
@@ -0,0 +1,103 @@
+<?php
+/** -------------------------------------------------------------------------------------------------------------------- ** 
+/** -------------------------------------------------------------------------------------------------------------------- ** 
+/** ---																																					--- **
+/** --- 										------------------------------------------------											--- **
+/** ---													{ load_attributes_names.php } 														--- **
+/** --- 										------------------------------------------------											--- **
+/** ---																																					--- **
+/** ---		TAB SIZE			: 3																													--- **
+/** ---																																					--- **
+/** ---		AUTEUR			: Nicolas DUPRE																									--- **
+/** ---																																					--- **
+/** ---		RELEASE			: 24.04.2017																										--- **
+/** ---																																					--- **
+/** ---		FILE_VERSION	: 1.0 NDU																											--- **
+/** ---																																					--- **
+/** ---																																					--- **
+/** --- 														---------------------------														--- **
+/** ---																{ G I T H U B }																--- **
+/** --- 														---------------------------														--- **
+/** ---																																					--- **
+/** ---		Automatize url?ts=3 :																												--- **
+/** ---																																					--- **
+/** ---			https://chrome.google.com/webstore/detail/tab-size-on-github/ofjbgncegkdemndciafljngjbdpfmbkn/related	--- **
+/** ---																																					--- **
+/** ---																																					--- **
+/** --- 														---------------------------														--- **
+/** ---															{ C H A N G E L O G }															--- **
+/** --- 														---------------------------														--- **
+/** ---																																					--- **
+/** ---																																					--- **
+/** ---		VERSION 1.0 : 24.04.2017 : NDU																									--- **
+/** ---		------------------------------																									--- **
+/** ---			- Première release																												--- **
+/** ---																																					--- **
+/** -------------------------------------------------------------------------------------------------------------------- **
+/** -------------------------------------------------------------------------------------------------------------------- **
+
+	Requirements :
+	--------------
+
+	Input Params :
+	--------------
+	
+	Output Params :
+	---------------
+
+	Objectif du script :
+	---------------------
+	
+	Description fonctionnelle :
+	----------------------------
+
+/** -------------------------------------------------------------------------------------------------------------------- **
+/** -------------------------------------------------------------------------------------------------------------------- **/
+function load_attributes_names($lang){
+	/** > Accession au PDO **/
+	global $PDO;
+	
+	/** > Déclaration des variables **/
+	$query;					// STRING			:: Requête SQL à jouer
+	$pQuery;					// PDOStatement	:: Requête SQL Préparée depuis $query
+	$faQuery;				// ARRAY				:: Donnée fetchée de $pQuery
+	$attributes_names;	// ARRAY				:: Liste des attributs
+	
+	/** > Initialisation des variables **/
+	$attributes_names = Array();
+	
+	
+	/** > Traitement **/
+	//--- Composition de la requête SQL
+	$query = "
+	SELECT
+		AN.ID,
+		AN.TAG,
+		AN.NAME
+		
+	FROM ATTRIBUTES_NAMES AS AN
+	
+	WHERE
+		AN.LANG = :lang
+	";
+	
+	
+	/** > Execution de la requête SQL **/
+	$pQuery = $PDO->prepare($query);
+	$pQuery->execute(Array(
+		":lang" => $lang
+	));
+	
+	
+	/** > Traitement des données **/
+	while($faQuery = $pQuery->fetch(PDO::FETCH_ASSOC)){
+		$attributes_names[] = Array(
+			"ATTRIBUTE_ID" => $faQuery["ID"],
+			"ATTRIBUTE_TAG" => $faQuery["TAG"],
+			"ATTRIBUTE_NAME" => $faQuery["NAME"]
+		);
+	}
+	
+	return $attributes_names;
+}
+?>
